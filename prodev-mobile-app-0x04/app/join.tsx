dd94be700095f697d71f5b00dbd382f1468a8db6
EniolaCodes <adebimpeniola@gmail.com>
2026-01-13 12:55:49 +0100

    navigating using stack navigation patterns

---
+++ prodev-mobile-app-0x04/app/join.tsx
@@ -0,0 +1,116 @@
+import { Text, TextInput, View, TouchableOpacity, Image } from "react-native";
+import { styles } from "@/styles/_joinstyle";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import { FACEBOOKLOGO, GOOGLELOGO } from "@/constants";
+import { useRouter } from "expo-router";
+
+export default function Index() {
+  const router = useRouter();
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View
+          style={{
+            ...styles.formGroup,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            gap: 10,
+          }}
+        >
+          <Ionicons
+            name="arrow-back"
+            size={25}
+            onPress={() => router.push("/")}
+          />
+          <Image source={require("@/assets/images/logo-green.png")} />
+        </View>
+        <Text style={styles.titleText}>Create your</Text>
+        <Text style={styles.titleText}>Account</Text>
+        <Text style={styles.subText}>
+          Enter your email and password to sign in.
+        </Text>
+
+        <View style={styles.formGroup}>
+          <View>
+            <Text style={styles.signupTitleText}>Email</Text>
+            <TextInput
+              keyboardType="email-address"
+              style={styles.formControl}
+            />
+          </View>
+          <View style={{ marginTop: 20 }}>
+            <Text style={styles.signupTitleText}>Password</Text>
+            <TextInput
+              keyboardType="email-address"
+              style={styles.formControl}
+            />
+          </View>
+        </View>
+        <Text style={styles.forgotPasswordText}>Forgot password?</Text>
+
+        <TouchableOpacity style={styles.secondaryButton}>
+          <Text style={styles.secondaryButtonText}>Create</Text>
+        </TouchableOpacity>
+
+        <View style={styles.dividerGroup}>
+          <View style={styles.divider}></View>
+          <Text style={styles.dividerText}>OR</Text>
+          <View style={styles.divider}></View>
+        </View>
+
+        <View
+          style={{
+            flexDirection: "column",
+            gap: 10,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <TouchableOpacity style={styles.iconsection}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+              }}
+            >
+              <Image source={GOOGLELOGO} />
+              <Text style={styles.signupTitleText}>Continue with Google</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.iconsection}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+              }}
+            >
+              <Image source={FACEBOOKLOGO} />
+              <Text style={styles.signupTitleText}>Continue with Facebook</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            marginTop: 20,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: 5,
+          }}
+        >
+          <Text style={styles.subText}>Don&apos;t have an account?</Text>
+          <Text
+            style={styles.signupSubTitleText}
+            onPress={() => router.push("/signin")}
+          >
+            Join now
+          </Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
